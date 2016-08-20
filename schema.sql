6f8c0d0b1ae1c691bf848ee3f0990fc588056557
sk89q <the.sk89q@gmail.com>
2016-08-20 00:53:04 -0700

    Delete outdated schema.sql.

--- schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `alias` (
-  `server_id` varchar(255) NOT NULL,
-  `alias` varchar(255) NOT NULL,
-  `command` text NOT NULL,
-  PRIMARY KEY (`server_id`,`alias`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
