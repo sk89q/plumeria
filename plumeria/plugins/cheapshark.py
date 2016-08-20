f29ed56ce6552358d1d24b60e737d7c8e9ad2184
sk89q <the.sk89q@gmail.com>
2016-08-20 01:44:23 -0700

    Move CheapShark API into plugin file.

--- plumeria/plugins/cheapshark.py
+++ plumeria/plugins/cheapshark.py
@@ -1,7 +1,20 @@
+import collections
+
 from plumeria.command import commands, CommandError
 from plumeria.message import Response
 from plumeria.util.ratelimit import rate_limit
-from plumeria.api.cheapshark import CheapShark
+from ..util.http import BaseRestClient
+
+Deal = collections.namedtuple("Deal", "id name price url")
+
+
+class CheapShark(BaseRestClient):
+    async def search(self, query):
+        json = await self.request("get", "http://www.cheapshark.com/api/1.0/games", params=dict(title=query, limit=1))
+        if len(json):
+            url = "http://www.cheapshark.com/redirect?dealID={}".format(json[0]['cheapestDealID'])
+            return Deal(json[0]['cheapestDealID'], json[0]['external'], json[0]['cheapest'], url)
+
 
 cheapshark = CheapShark()
 
