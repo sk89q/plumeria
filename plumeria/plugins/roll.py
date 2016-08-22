d554c74da69063d87d6f8b054f13adc0cd2da158
sk89q <the.sk89q@gmail.com>
2016-08-21 21:33:51 -0700

    Add 8ball.

--- plumeria/plugins/roll.py
+++ plumeria/plugins/roll.py
@@ -3,6 +3,37 @@ import random
 import dice
 from plumeria.command import commands, CommandError
 
+EIGHT_BALL_RESPONSES = (
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+)
+
+
+@commands.register('8ball', category='Utility')
+async def eight_ball(message):
+    """
+    Chooses a random response from an 8-ball.
+    """
+    return "{} **{}**".format(message.content, random.choice(EIGHT_BALL_RESPONSES)).strip()
+
 
 @commands.register('roll', category='Utility')
 async def roll(message):
