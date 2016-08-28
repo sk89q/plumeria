9c6fac2961a866dc801daa12209147ee653af438
sk89q <the.sk89q@gmail.com>
2016-08-28 16:47:12 -0700

    Add additional Minecraft commands.

--- plumeria/plugins/minecraft.py
+++ plumeria/plugins/minecraft.py
@@ -9,7 +9,7 @@ MINECRAFT_STATUS_URL = "http://xpaw.ru/mcstatus/status.json"
 HEADERS = {"Cookie": "__cfduid=d100ef4ed084175f2382f35613cb9c9dc1458598283"}
 VALID_NAME = re.compile("^[A-Za-z0-9_]{1,30}$")
 VALID_UUID = re.compile(
-    "(?:[A-Fa-f0-9]{32}|[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12})")
+    "(?:[A-Fa-f0-9]{32}|[A-Fa-f0-9]{8}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{4}-?[A-Fa-f0-9]{12})")
 
 
 def validate_name(name):
@@ -24,6 +24,19 @@ def validate_uuid(uuid):
     if m:
         return m.group(0).replace("-", "")
     raise CommandError("Invalid UUID given")
+
+
+async def name_to_uuid(name):
+    m = VALID_UUID.search(name)
+    if m:
+        return m.group(0)
+    name = validate_name(name)
+    r = await http.get("https://mcapi.ca/uuid/player/" + name)
+    results = r.json()
+    if len(results):
+        return results[0]['uuid_formatted']
+    else:
+        raise CommandError("user not found")
 
 
 @commands.register("minecraft status", "mcstatus", category="Minecraft")
@@ -62,13 +75,78 @@ async def mc_uuid(message):
     """
     name = validate_name(message.content)
     try:
-        r = await http.get("https://mcapi.ca/uuid/player/" + name)
-        results = r.json()
-        if len(results):
-            return Response(results[0]['uuid_formatted'])
-        else:
-            raise CommandError("no results")
+        return await name_to_uuid(name)
     except http.BadStatusCodeError as e:
         if e.http_code == 404:
             raise CommandError("No user found by that name")
         raise CommandError("API returned an error code")
+
+
+@commands.register("minecraft body", "mcbody", category="Minecraft")
+@rate_limit()
+async def mc_body(message):
+    """
+    Get the rendered 3D body for a Minecraft user with his or her skin.
+
+    Example::
+
+        /mcbody sk89q
+
+    """
+    return "https://crafatar.com/renders/body/{}".format(await name_to_uuid(message.content))
+
+
+@commands.register("minecraft head", "mchead", category="Minecraft")
+@rate_limit()
+async def mc_head(message):
+    """
+    Get the rendered 3D head for a Minecraft user with his or her skin.
+
+    Example::
+
+        /mchead sk89q
+
+    """
+    return "https://crafatar.com/renders/head/{}".format(await name_to_uuid(message.content))
+
+
+@commands.register("minecraft face", "mcface", category="Minecraft")
+@rate_limit()
+async def mc_face(message):
+    """
+    Get the face for a Minecraft user with his or her skin.
+
+    Example::
+
+        /mcface sk89q
+
+    """
+    return "https://crafatar.com/avatars/{}".format(await name_to_uuid(message.content))
+
+
+@commands.register("minecraft skin", "mcskin", category="Minecraft")
+@rate_limit()
+async def mc_skin(message):
+    """
+    Get the skin for a Minecraft user.
+
+    Example::
+
+        /mcskin sk89q
+
+    """
+    return "https://crafatar.com/skins/{}".format(await name_to_uuid(message.content))
+
+
+@commands.register("minecraft cape", "mccape", category="Minecraft")
+@rate_limit()
+async def mc_cape(message):
+    """
+    Get the cape for a Minecraft user.
+
+    Example::
+
+        /mccape sk89q
+
+    """
+    return "https://crafatar.com/capes/{}".format(await name_to_uuid(message.content))
