b0358b707eb55f4398b74d5d08f04bb8928f743e
sk89q <the.sk89q@gmail.com>
2016-08-28 12:43:24 -0700

    Close menu on click and use a scroll animation.

--- plumeria/static/docs.js
+++ plumeria/static/docs.js
@@ -1,3 +1,15 @@
 $(".doc-nav-menu").on("click", function (event) {
     $("body").toggleClass("sidebar-open");
 });
+$(".doc-nav").on("click", "a", function (event) {
+    $("body").removeClass("sidebar-open");
+    if (this.getAttribute("href").match(/^#/)) {
+        var target = $(this.getAttribute("href"));
+        if (target.length > 0) {
+            $('html, body').animate({
+                scrollTop: target.offset().top - 70
+            }, 200);
+            event.preventDefault();
+        }
+    }
+});
