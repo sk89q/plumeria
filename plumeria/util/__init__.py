ed0b33dc0866100c2bede3579711af761d5cb159
sk89q <the.sk89q@gmail.com>
2016-08-28 06:15:07 -0700

    Fix mimetype detection to handle more types.

--- plumeria/util/__init__.py
+++ plumeria/util/__init__.py
@@ -3,9 +3,12 @@ MIME_TYPES = {
     '.jpg': 'image/jpeg',
     '.jpeg': 'image/jpeg',
     '.gif': 'image/gif',
+    '.txt': 'text/plain',
 }
 
 
 def to_mimetype(ext):
     if ext.lower() in MIME_TYPES:
         return MIME_TYPES[ext.lower()]
+    else:
+        return "application/octet-stream"
