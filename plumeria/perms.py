8c0a657f16f42197f9397c9d473a2ad9aa9d42d0
sk89q <the.sk89q@gmail.com>
2016-08-29 21:54:22 -0700

    Add has_role_in() to perms.

--- plumeria/perms.py
+++ plumeria/perms.py
@@ -6,6 +6,13 @@ from .command import AuthorizationError
 owner_ids = config.create("perms", "admin_users", set_of(str), fallback="")
 
 
+def has_role_in(user, roles):
+    for role in user.roles:
+        if role.name in roles:
+            return True
+    return False
+
+
 def is_owner(user):
     return user.id in owner_ids()
 
@@ -39,3 +46,20 @@ def server_admins_only(f):
 
     wrapper.server_admins_only = True
     return wrapper
+
+
+def roles_only(*roles):
+    roles = set(roles)
+
+    def inner(f):
+        @wraps(f)
+        async def wrapper(message, *args, **kwargs):
+            if has_role_in(message.author, roles):
+                return await f(message, *args, **kwargs)
+            else:
+                raise AuthorizationError()
+
+        wrapper.server_admins_only = True
+        return wrapper
+
+    return inner
