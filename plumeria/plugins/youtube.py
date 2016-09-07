023473de2cc189c5d72f97c28cdbaee8e0b1a630
Boris Lachev <boris.lachevbg00@gmail.com>
2016-09-08 00:57:18 +0300

    Added command for getting a random overused Minecraft let's play EDM song from NCS.

--- plumeria/plugins/youtube.py
+++ plumeria/plugins/youtube.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from plumeria.api.youtube import YouTube
 from plumeria.command import commands, CommandError, channel_only
 from plumeria.message import Response
@@ -22,3 +24,23 @@ async def yt(message):
             return Response(videos[0].url)
         else:
             raise CommandError("No video found!")
+
+
+@commands.register('mcmusic', 'overusedmusic', cost=2, category='Search')
+@rate_limit()
+async def ytmcmusic():
+    """
+
+    Search NCS for overused music, mostly used in Minecraft let's play intros.
+
+    Example::
+        /mcmusic
+    """
+    videos = await youtube.searchExtended("UC_aEa8K-EOJ3D6gOs7HcyNg", "viewcount", "50")
+    if len(videos):
+        index = randint(0, len(videos))
+        return Response(videos[index].url)
+    else:
+        raise CommandError("Error in lookup!")
+
+
