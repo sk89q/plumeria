037899b51d42f02dd76296d7551aa8da6df580ea
sk89q <the.sk89q@gmail.com>
2016-10-09 01:13:16 -0700

    Add bot control module.

---
+++ plumeria/plugins/bot_control.py
@@ -0,0 +1,33 @@
+from plumeria.command import commands, CommandError
+from plumeria.message.lists import build_list
+from plumeria.perms import owners_only
+from plumeria.transport import transports
+
+
+@commands.register('accept invite', category='Discord')
+@owners_only
+async def accept_invite(message):
+    """
+    Accept an invite to join a server.
+
+    Example::
+
+        /accept invite https://discord.gg/00000
+    """
+    url = message.content.strip()
+    results = []
+    if not len(url):
+        raise CommandError("Supply an invite URL.")
+    for transport in transports.transports.values():
+        if hasattr(transport, 'accept_invite'):
+            try:
+                await transport.accept_invite(url)
+                results.append((transport.id, 'Success \N{WHITE HEAVY CHECK MARK}'))
+            except Exception as e:
+                results.append((transport.id, '\N{WARNING SIGN} {}'.format(str(e))))
+        else:
+            results.append((transport.id, "\N{WARNING SIGN} No support for invite links"))
+    if len(results):
+        return build_list(["**{}:** {}".format(e[0], e[1]) for e in results])
+    else:
+        raise CommandError("No transports available.")
